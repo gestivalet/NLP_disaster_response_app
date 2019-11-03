f320cdb8e81a71385af0ff34fe5edb1f7206970a
Gestivalet <gestivalet@gmail.com>
2019-11-03 17:22:22 +0200

    ETL workflow completed - jupyter and pyscript

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,45 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    """
+    Loads and concatenates the messages and categories csv files into a pandas DataFrame.
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(left=messages, right=categories, on='id')
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Organize categories values and remove duplicates from final dataframe.
+    """
 
+    # organize catefories column
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0,:]
+    categories.columns = [val[:-2] for val in row]
 
-def save_data(df, database_filename):
-    pass  
+    for column in categories:
+        # set each value to be the last character of the string
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(lambda x: float(x.split('-')[1]))
+    
+
+    # drop old categories column from original dataframe
+    df.drop('categories', axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace=True)
+
+    return df
+
+def save_data(df, database_filepath):
+    engine = create_engine(database_filepath)
+    df.to_sql('main_table', engine, index=False)
 
 
 def main():
